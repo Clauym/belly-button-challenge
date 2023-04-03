be54f80b5032dd9fece1be860104a0b7826c7969
Clauym <claudia.yurrita@gmail.com>
2023-04-03 13:54:29 -0500

    First update. Dropdown, bar graph and demographics ready, bubble 90%

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # belly-button-challenge
+## Challenge #14
+
+This folder contains  
+
+
+
+----
+----
---
+++ StarterCode/static/js/bonus.js
@@ -0,0 +1,10 @@
+//1. Use the D3 library to read in samples.json
+// from the URL
+
+//const url ="https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
+
+// Fetch the JSON data and console log it 
+//d3.json(url).then(samples=> {
+//    console.log(samples);
+//});
+
